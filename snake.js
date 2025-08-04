4b5b4a8e39d4b7a09c660b28e523a968006cc206
LucasFRodriguez <lucasfrz26@gmail.com>
2025-08-04 17:00:36 -0300

    snake-controles

--- snake.js
+++ snake.js
@@ -62,13 +62,13 @@ class Apple{
 }
 
 
-var canvas = document.getElementsById("canvas")
+var canvas = document.getElementById("canvas")
 
-var snake = new Snake();
+var snake = new Snake(20, 20, 20);
 
 var apple = new Apple();
 
-var canvasContect = canvas.getContext('2d');
+var canvasContext = canvas.getContext('2d');
 
 window.onload = ()=>{
     gameLoop();
@@ -84,7 +84,10 @@ function show(){
 }
 
 function update(){
-
+    canvasContext.clearRect(0,0, canvas.whidth, canvas.height)
+    console.log("UPDATE")
+    snake.move()
+    
 }
 
 function draw(){
@@ -94,8 +97,33 @@ function draw(){
         createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5,
             snake.size - 5, snake.size - 5, 'white')
     }
+
+    canvasContext.font = "20px Arial"
+    canvasContext.fillStyle = "#00FF42"
+    canvasContext.fillText("Score: ",(snake.tail.length +1),
+        canvas.width -120, 18 );
+    createRect(apple.x, apple.y,apple.size, apple.size, apple.color)
 }
 
-function createRect( x, y, width, height ){
-
+function createRect( x, y, width, height, color ){
+    canvasContext.fillStyle = color
+    canvasContext.fillRect(x,y,width,height)
 }
+
+window.addEventListener("keydown", (event)=>{
+    setTimeout(()=>{
+        if(event.keyCode == 37 && snake.rotateX != 1){
+            snake.rotateX = -1
+            snake.rotateY = 0;
+        }else if(event.keyCode == 38 && snake.rotateY != 1){
+            snake.rotateX = 0
+            snake.rotateY = -1;
+        }else if(event.keyCode == 39 && snake.rotateX != -1){
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        }else if(event.keyCode == 40 && snake.rotateY != -1){
+            snake.rotateX = 0
+            snake.rotateY = 1;
+        }
+    }, 1)
+})
